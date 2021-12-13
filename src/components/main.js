12ad92d904fb4befabe09f05829c4d2b723f1bc4
RoySamson-stack <rsam9009@gmail.com>
2021-12-13 17:29:57 +0300

    adding the boilerplate for project

---
+++ src/components/main.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Forum(){
+  return(
+    <div className="forum-main">
+      <h1 className="forum-header">Forum</h1>
+      <div className="forum-container">
+        <input type="" className="forum-post"/>
+        <button className="forum-btn">Create post</button>
+      </div>
+    </div>
+  )
+}
